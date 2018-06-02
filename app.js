4dbf857323acb6be14ba982ed5af49e134a93b51
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-06-02 09:01:10 +0800

    changed server port

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const user = require("./routes/user");
 app.use("/user", user);
 
 // set port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`we are live at ${port}`);
 });
